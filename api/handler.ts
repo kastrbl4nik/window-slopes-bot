7648a97b56c54b578d90b412f7d7acd36e88bb4e
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 23:25:33 +0200

    Add error handling to handler

--- api/handler.ts
+++ api/handler.ts
@@ -7,7 +7,9 @@ export default function handler(request : any, response : any) {
   console.log("[BOT INFO]: ");
   console.log(bot);
 
-  bot.sendMessage(request.body.message.chat.id, '[From handler]: HELLO WORLD');
+  bot.sendMessage(request.body.message.chat.id, '[From handler]: HELLO WORLD')
+    .then((data) => {console.log('[SUCCESS]: message sent')})
+    .catch((err) => {console.log('[ERROR]: '); console.log(err)});
 
   response.status(200).json({
     body: request.body,
