841290f0124be83ff17304ff7855bc9317d2119e
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 21:06:26 +0200

    Add console.log() to handler.ts

--- api/handler.ts
+++ api/handler.ts
@@ -3,6 +3,7 @@ import bot from '../src/bot';
 export default function handler(request : any, response : any) {
   bot.receiveUpdates([request.body]);
   console.log(request.body);
+  console.log(bot.getWebhookInfo());
 
   response.status(200).json({
     body: request.body,
