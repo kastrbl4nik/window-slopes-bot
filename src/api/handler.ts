8fdea5fbc35e6fdbf69a6bb7a778458b37d24909
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 20:30:06 +0200

    Add vercel.json

--- src/api/handler.ts
+++ src/api/handler.ts
@@ -1,4 +1,4 @@
-export default function handler(request, response) {
+export default function handler(request : any, response : any) {
   console.log(request.body);
   response.status(200).json({
     body: request.body,
