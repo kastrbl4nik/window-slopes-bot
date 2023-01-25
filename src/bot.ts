b88d268ad4a65963c6389264eaa4b410dfc30cff
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 23:00:41 +0200

    Declare webhook in the bot instance

--- src/bot.ts
+++ src/bot.ts
@@ -1,14 +1,16 @@
 import TeleBot from 'telebot';
 
-const bot = new TeleBot(process.env.TELEGRAM_TOKEN as string);
+const bot = new TeleBot({
+    token: process.env.TELEGRAM_TOKEN as string,
+    webhook: {
+        url: `https://${process.env.VERCEL_ENV}/api/handler`,
+    }
+});
 
 bot.on('/start', msg => {
     return bot.sendMessage(msg.chat.id, "HELLO WORLD");
 });
 
-if(process.env.NODE_ENV === "debug")
-    bot.start();
-else
-    bot.setWebhook(`https://${process.env.VERCEL_ENV}/api/handler`).catch((err) => console.log(err));
+bot.start();
 
 export default bot;
