ec36e7a7612438a219e0aec3d33cd161f18fe7f1
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 21:30:08 +0200

    Set webhook of telebot

--- api/handler.ts
+++ api/handler.ts
@@ -1,12 +1,11 @@
 import bot from '../src/bot';
 
-
-
 export default function handler(request : any, response : any) {
+  bot.setWebhook(`https://${process.env.VERCEL_ENV}/api/handler`);
   bot.receiveUpdates([request.body]);
   console.log("[REQUEST BODY]: ");
   console.log(request.body);
-  console.log("[RECEIVED BOT]: ");
+  console.log("[BOT INFO]: ");
   console.log(bot);
 
   response.status(200).json({
