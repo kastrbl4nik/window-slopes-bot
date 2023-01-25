7e1d0e8fe3fa1b78059ba24017c53b31d0051530
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 23:47:12 +0200

    Add setWebhook to /api

--- api/handler.ts
+++ api/handler.ts
@@ -1,19 +1,16 @@
 import bot from '../src/bot';
 
-export default function handler(request : any, response : any) {
-  bot.receiveUpdates([request.body]);
+export default async ({ body } : any, { json } : any) =>
+    json(body && body.update_id ? await bot.receiveUpdates([body]) : {status: false})
+/*export default function handler(request : any, response : any) {
   console.log("[REQUEST BODY]: ");
   console.log(request.body);
   console.log("[BOT INFO]: ");
   console.log(bot);
-
-  bot.sendMessage(request.body.message.chat.id, '[From handler]: HELLO WORLD')
-    .then((data) => {console.log('[SUCCESS]: message sent')})
-    .catch((err) => {console.log('[ERROR]: '); console.log(err)});
 
   response.status(200).json({
     body: request.body,
     query: request.query,
     cookies: request.cookies,
   });
-}
+}*/
---
+++ api/setWebhook.ts
@@ -0,0 +1,7 @@
+import bot from '../src/bot';
+
+const isDev = process.env.VERCEL_ENV === 'development';
+const webHookURL = host => `https://${host}/api/telegram.mjs`;
+
+export default async ({body: {url}, headers}, {json}) =>
+    json(await bot.setWebhook(isDev && url ? url : webHookURL(headers['x-forwarded-host'])).catch(e => e));
