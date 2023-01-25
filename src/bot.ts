72ca9f02fb9505709ee9877ab63636a68559f4c5
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 19:54:43 +0200

    Remove redundunt code from bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -8,11 +8,3 @@ bot.on('/start', msg => {
 
 if(process.env.NODE_ENV === "debug")
     bot.start();
-
-module.exports = async(request : any, response: any) => {
-    response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-    });
-}
