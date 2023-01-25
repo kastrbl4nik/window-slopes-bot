d93cdf3fa14f926f95ea6eeff1ac00eb3ffab646
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 17:19:57 +0200

    Move handler to /api

--- src/handler.ts
+++
@@ -1,7 +0,0 @@
-module.exports = async (request : any, response : any) => {
-    response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-    });
-};
