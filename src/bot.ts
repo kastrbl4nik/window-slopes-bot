96f9cfd1aa81cb93e9956ed511da4d4e1cecf110
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 23:13:55 +0200

    Set webhook instead of polling

--- src/bot.ts
+++ src/bot.ts
@@ -1,16 +1,12 @@
 import TeleBot from 'telebot';
 
-const bot = new TeleBot({
-    token: process.env.TELEGRAM_TOKEN as string,
-    webhook: {
-        url: `https://${process.env.VERCEL_URL}/api/handler`,
-    }
-});
+const bot = new TeleBot(process.env.TELEGRAM_TOKEN as string);
 
 bot.on('/start', msg => {
     return bot.sendMessage(msg.chat.id, "HELLO WORLD");
 });
 
-bot.start();
+//bot.start();
+bot.setWebhook(`https://${process.env.VERCEL_URL}/api/handler`);
 
 export default bot;
