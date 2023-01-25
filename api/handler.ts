4fc0c47d3046ce696efd805e9d7498e074e6d771
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-25 21:23:17 +0200

    Edit handler.js

--- api/handler.ts
+++ api/handler.ts
@@ -1,15 +1,13 @@
 import bot from '../src/bot';
 
+
+
 export default function handler(request : any, response : any) {
   bot.receiveUpdates([request.body]);
   console.log("[REQUEST BODY]: ");
   console.log(request.body);
-  console.log("[WEBHOOK INFO]: ");
-  bot.getWebhookInfo().then((data) => {
-    console.log(data);
-  }).catch((error) => {
-    console.log(error);
-  })
+  console.log("[RECEIVED BOT]: ");
+  console.log(bot);
 
   response.status(200).json({
     body: request.body,
