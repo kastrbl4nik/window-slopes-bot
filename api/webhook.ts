370dcbf23396e7290606770a7a6fc42f2791f4f7
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-26 15:36:33 +0200

    Add webhook.ts

---
+++ api/webhook.ts
@@ -0,0 +1,8 @@
+import bot from '../src/bot';
+
+export default async (request : any, response : any) => {
+	console.debug(request.body);
+  	return response.status(200).json(
+		await bot.setWebhook(`https://window-slopes-bot.vercel.app/api/handler`)
+	);
+}
--- src/bot.ts
+++ src/bot.ts
@@ -6,6 +6,9 @@ bot.on('/start', msg => {
     return bot.sendMessage(msg.chat.id, "HELLO WORLD");
 });
 
-bot.start();
+if(process.env.VERCEL_ENV != "production")
+    bot.start(); // Use long polling instead of webhook
+else
+    bot.setWebhook(`https://window-slopes-bot.vercel.app/api/handler`);
 
 export default bot;
