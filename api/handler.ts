9980a96c838243e9973a4cee9913b65b2754f006
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-26 15:24:09 +0200

    Add debug log to handler.ts

--- api/handler.ts
+++ api/handler.ts
@@ -1,8 +1,7 @@
 import bot from '../src/bot';
 
 export default async (request : any, response : any) => {
-	console.log(request);
-	console.log(response);
+	console.debug(request.body);
   	return response.status(200).json(
 		await bot.receiveUpdates([request.body])
 	);
