aa10ecee8b0f79194405428215add422ae4a3156
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-26 15:39:51 +0200

    Add promise rejection handling in webhook.ts

--- api/webhook.ts
+++ api/webhook.ts
@@ -3,6 +3,6 @@ import bot from '../src/bot';
 export default async (request : any, response : any) => {
 	console.debug(request.body);
   	return response.status(200).json(
-		await bot.setWebhook(`https://window-slopes-bot.vercel.app/api/handler`)
+		await bot.setWebhook(`https://window-slopes-bot.vercel.app/api/handler`).catch(e => console.error(e))
 	);
 }
