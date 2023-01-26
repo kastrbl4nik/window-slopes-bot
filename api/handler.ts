0668d40286f914b21c8b5d8733bb7127998d5061
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-26 13:58:35 +0200

    Update handler.ts

--- api/handler.ts
+++ api/handler.ts
@@ -1,5 +1,7 @@
 import bot from '../src/bot';
 
 export default async ({body} : any, {json} : any) => {
+	console.log(body);
+	console.log(json);
   	return json(body && body.update_id ? await bot.receiveUpdates([body]) : { status: false });
 }
