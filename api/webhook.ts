eae22535d4a10891f532fca3da9269a4d6bf0ca1
Mikita Shunko <kastrbl4nik@gmail.com>
2023-01-26 15:43:50 +0200

    Add then clause to webhook.ts

--- api/webhook.ts
+++ api/webhook.ts
@@ -3,6 +3,8 @@ import bot from '../src/bot';
 export default async (request : any, response : any) => {
 	console.debug(request.body);
   	return response.status(200).json(
-		await bot.setWebhook(`https://window-slopes-bot.vercel.app/api/handler`).catch(e => console.error(e))
+		await bot.setWebhook(`https://window-slopes-bot.vercel.app/api/handler`)
+        .then(i => console.info(i))
+        .catch(e => console.error(e))
 	);
 }
